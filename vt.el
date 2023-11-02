75f3fe9fe2dc8aeb334495f7f54b56aa6426c828
yaxley-peaks <epiclycoolgaemer@gmail.com>
2023-11-02 22:22:05 +0530

    Changed metadata

--- vt.el
+++ vt.el
@@ -7,8 +7,8 @@
 ;; Created: November 02, 2023
 ;; Modified: November 02, 2023
 ;; Version: 0.0.1
-;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
-;; Homepage: https://github.com/yax/vt
+;; Keywords: convenience
+;; Homepage: https://github.com/yaxley-peaks/vt
 ;; Package-Requires: ((emacs "24.3"))
 ;;
 ;; This file is not part of GNU Emacs.
