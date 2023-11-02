5acd4d89be47f9f113c0bddadf1bdc278d8c4033
yaxley-peaks <epiclycoolgaemer@gmail.com>
2023-11-02 21:51:08 +0530

    init commit

---
+++ vt.el
@@ -0,0 +1,25 @@
+;;; vt.el --- Browse /vt/ -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2023 Yaxley Peaks
+;;
+;; Author: Yaxley Peaks <epiclycoolgaemer@gmail.com>
+;; Maintainer: Yaxley Peaks <epiclycoolgaemer@gmail.com>
+;; Created: November 02, 2023
+;; Modified: November 02, 2023
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/yax/vt
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+
+
+(provide 'vt)
+;;; vt.el ends here
